37597022bc40049c934959ae1950fbf322e23157
jamalms <95721607+jamalms@users.noreply.github.com>
2022-06-23 16:46:51 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,5 @@
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(accumulator, array) => {
+		console.log('array', array);
+		console.log('accumulator', accumulator);
+	return accumulator.concat(array) }, []);
